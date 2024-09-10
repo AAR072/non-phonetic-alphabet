f8027e482521a1d70faa32d0f0e4cf1d7609bea7
Ali-Akber Raza <aaoblivion@gmail.com>
2024-09-09 22:15:28 -0400

    Added ibm carbon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,54 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+  
+<script>
+import "carbon-components-svelte/css/g100.css";
+import { TextInput } from "carbon-components-svelte";
+  import {
+    Header,
+    HeaderNav,
+    HeaderNavItem,
+    HeaderNavMenu,
+    SkipToContent,
+    Content,
+    Grid,
+    Row,
+    Column,
+  } from "carbon-components-svelte";
+
+  let isSideNavOpen = false;
+  let inputText = ""
+</script>
+
+<Header company="IBM" platformName="Carbon Svelte" bind:isSideNavOpen>
+  <svelte:fragment slot="skip-to-content">
+    <SkipToContent />
+  </svelte:fragment>
+  <HeaderNav>
+    <HeaderNavItem href="/" text="Link 1" />
+    <HeaderNavItem href="/" text="Link 2" />
+    <HeaderNavItem href="/" text="Link 3" />
+    <HeaderNavMenu text="Menu">
+      <HeaderNavItem href="/" text="Link 1" />
+      <HeaderNavItem href="/" text="Link 2" />
+      <HeaderNavItem href="/" text="Link 3" />
+    </HeaderNavMenu>
+  </HeaderNav>
+</Header>
+
+<Content>
+  <Grid>
+    <Row>
+      <Column>
+      <h1>Troll Scammers</h1>
+      <br>
+      <h2>Try now<h2>
+      <br>
+      <TextInput size="xl" 
+      bind:value={inputText}
+       on:keyup={() => alert(name)} placeholder="John Doe"/>
+      <br>
+      <h2>Or see some examples<h2>
+      </Column>
+    </Row>
+  </Grid>
+</Content>
+
